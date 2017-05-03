601d2bb66d59c294668616e50e131d0cbb235707
Ravindra Kumar <ravidsrk@gmail.com>
2017-05-04 00:29:18 +0530

    feat(DaggerModules): Converted dagger modules to Kotlin

---
+++ app/src/main/java/in/mvpstarter/sample/injection/module/FragmentModule.kt
@@ -0,0 +1,30 @@
+package `in`.mvpstarter.sample.injection.module
+
+import android.app.Activity
+import android.content.Context
+import android.support.v4.app.Fragment
+
+import dagger.Module
+import dagger.Provides
+import `in`.mvpstarter.sample.injection.ActivityContext
+
+@Module
+class FragmentModule(private val mFragment: Fragment) {
+
+    @Provides
+    internal fun providesFragment(): Fragment {
+        return mFragment
+    }
+
+    @Provides
+    internal fun provideActivity(): Activity {
+        return mFragment.activity
+    }
+
+    @Provides
+    @ActivityContext
+    internal fun providesContext(): Context {
+        return mFragment.activity
+    }
+
+}
