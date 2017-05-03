1850d2a2b07378d7c13aad9a5522b079741110f1
Ravindra Kumar <ravidsrk@gmail.com>
2017-05-03 19:29:02 +0530

    feat(Dagger Component): Converted all the Dagger Components to Kotlin

---
+++ app/src/main/java/in/mvpstarter/sample/injection/component/ConfigPersistentComponent.kt
@@ -0,0 +1,25 @@
+package `in`.mvpstarter.sample.injection.component
+
+import dagger.Component
+import `in`.mvpstarter.sample.injection.ConfigPersistent
+import `in`.mvpstarter.sample.injection.module.ActivityModule
+import `in`.mvpstarter.sample.injection.module.FragmentModule
+import `in`.mvpstarter.sample.ui.base.BaseActivity
+import `in`.mvpstarter.sample.ui.base.BaseFragment
+
+/**
+ * A dagger component that will live during the lifecycle of an Activity or Fragment but it won't
+ * be destroy during configuration changes. Check [BaseActivity] and [BaseFragment] to
+ * see how this components survives configuration changes.
+ * Use the [ConfigPersistent] scope to annotate dependencies that need to survive
+ * configuration changes (for example Presenters).
+ */
+@ConfigPersistent
+@Component(dependencies = arrayOf(ApplicationComponent::class))
+interface ConfigPersistentComponent {
+
+    fun activityComponent(activityModule: ActivityModule): ActivityComponent
+
+    fun fragmentComponent(fragmentModule: FragmentModule): FragmentComponent
+
+}
---
+++ app/src/main/java/in/mvpstarter/sample/injection/component/FragmentComponent.kt
@@ -0,0 +1,12 @@
+package `in`.mvpstarter.sample.injection.component
+
+import dagger.Subcomponent
+import `in`.mvpstarter.sample.injection.PerFragment
+import `in`.mvpstarter.sample.injection.module.FragmentModule
+
+/**
+ * This component inject dependencies to all Fragments across the application
+ */
+@PerFragment
+@Subcomponent(modules = arrayOf(FragmentModule::class))
+interface FragmentComponent
